82ab109026a686d7ffbfc3972ba98706b2e9cf23
Alberto <alberto.de.assumpcao@gmail.com>
2025-09-26 15:28:16 -0300

    question-2 refactoring and code review (adding game states)

--- Assets/_Custom/scripts/BoardPresenter.cs
+++ Assets/_Custom/scripts/BoardPresenter.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class BoardPresenter : MonoBehaviour
 {
+    public enum GameState{Gameplay, GameOver}
     [SerializeField]
-    private int initialPoints;
+    private int initialPoints = 0;
     [SerializeField]
     private int initialMoves = 5;
     [SerializeField]
@@ -15,6 +16,7 @@ public class BoardPresenter : MonoBehaviour
     [SerializeField]
     private GameObject gameOverPanel;
     
+    private GameState _gameState = GameState.Gameplay;
     private int _currentMoves;
     private int _currentPoints;
 
@@ -25,44 +27,52 @@ public class BoardPresenter : MonoBehaviour
 
     public void UpdateMoves(int value)
     {
-        _currentMoves -= value;
-        if (!movesText)
-        {
-            Debug.LogWarning("Points Text is null");
-            return;
-        }
-        
-        movesText.text = _currentMoves.ToString();
+        UpdateValue(value, movesText, ref _currentMoves);
         CheckForGameOver();
     }
     
     public void UpdatePoints(int value)
     {
-        _currentPoints += value;
-        if(!pointsText)
-        {
-            Debug.LogWarning("Moves Text is null");
-            return;
-        }
-        
-        pointsText.text = _currentPoints.ToString();
+        UpdateValue(value, pointsText, ref _currentPoints);
     }
 
     public void ResetGame()
     {
-        _currentMoves = initialMoves;
         _currentPoints = initialPoints;
-        UpdateMoves(0);
-        UpdatePoints(0);
+        pointsText.text = _currentPoints.ToString();
+        _currentMoves = initialMoves;
+        movesText.text = _currentMoves.ToString();
         if (gameOverPanel.activeInHierarchy)
         {
             gameOverPanel.SetActive(false);
         }
+        _gameState = GameState.Gameplay;
+    }
+
+    private void UpdateValue(int value, TextMeshProUGUI text, ref int currentValue)
+    {
+        if (_gameState != GameState.Gameplay)
+        {
+            return;
+        }
+        
+        currentValue += value;
+        if(!text)
+        {
+            Debug.LogWarning("Text Object is null");
+            return;
+        }
+        
+        text.text = currentValue.ToString();
     }
 
     private void CheckForGameOver()
     {
-        if (_currentMoves > 0) return;
+        if (_currentMoves > 0)
+        {
+            return;
+        }
         gameOverPanel.SetActive(true);
+        _gameState = GameState.GameOver;
     }
 }
