1441b6f49e06c11a43118a48e755b232327ed4ab
Alberto <alberto.de.assumpcao@gmail.com>
2025-09-26 14:59:09 -0300

    question-1 main scene and gameover panel with style and interfaces

---
+++ Assets/_Custom/scripts/BoardPresenter.cs
@@ -0,0 +1,10 @@
+using TMPro;
+using UnityEngine;
+
+public class BoardPresenter : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI pointsText;
+    [SerializeField]
+    private TextMeshProUGUI movesText;
+}
